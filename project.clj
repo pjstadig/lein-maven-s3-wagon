70ed025f196edc256dbe3a41ce0c7f81e6406692
Paul Stadig <paul@stadig.name>
2014-03-20 21:24:05 -0400

    Update description in project.clj.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject lein-maven-s3-wagon "0.2.0"
-  :description "FIXME: write description"
+  :description "A leiningen plugin for publishing to a private S3
+  maven repository."
   :url "http://github.com/pjstadig/lein-maven-s3-wagon/"
   :license {:name "Mozilla Public License, v. 2.0"
             :url "http://mozilla.org/MPL/2.0/"}
