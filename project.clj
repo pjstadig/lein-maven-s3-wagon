e08257de46fe6a5dfb4541762b7897d099c90c9d
Paul Stadig <paul@stadig.name>
2014-05-02 14:17:15 -0400

    Bump maven-s3-wagon to 1.3.3.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :url "http://github.com/pjstadig/lein-maven-s3-wagon/"
   :license {:name "Mozilla Public License, v. 2.0"
             :url "http://mozilla.org/MPL/2.0/"}
-  :dependencies [[pjstadig/maven-s3-wagon "1.3.2"]]
+  :dependencies [[pjstadig/maven-s3-wagon "1.3.3"]]
   :eval-in-leiningen true)
