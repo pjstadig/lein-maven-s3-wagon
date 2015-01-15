61afa659da5812d0af176415f2f115c8fdb9b577
Paul Stadig <paul@stadig.name>
2015-01-14 20:50:53 -0500

    Add some logging.

--- project.clj
+++ project.clj
@@ -4,13 +4,15 @@
   :url "http://github.com/pjstadig/lein-maven-s3-wagon/"
   :license {:name "Mozilla Public License, v. 2.0"
             :url "http://mozilla.org/MPL/2.0/"}
-  :repositories [["private" {:url "s3p://outpace-maven/releases/"
+  :repositories [["private" {:url "s3p://pjstadig-maven/releases/"
                              :username :env/aws_access_key_id
                              :password :env/aws_secret_access_key}]]
   :dependencies [[org.apache.maven.wagon/wagon-provider-api "2.2"]
                  [org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [pjstadig/maven-s3-wagon "1.3.3"]
                  [prismatic/schema "0.3.3"]
-                 [com.outpace/codd "0.1.0"]]
+                 [foo "0.1.0"]
+                 [log4j "1.2.17"]]
   :plugins [[lein-maven-s3-wagon "0.2.4-SNAPSHOT"]]
   :aot [pjstadig.maven.wagon.S3Wagon])
--- src/pjstadig/maven/wagon/S3Wagon.clj
+++ src/pjstadig/maven/wagon/S3Wagon.clj
@@ -1,4 +1,5 @@
 (ns pjstadig.maven.wagon.S3Wagon
+  (:require [clojure.tools.logging :as log])
   (:import (org.apache.maven.wagon Wagon))
   (:gen-class
    :implements [org.apache.maven.wagon.Wagon]
@@ -6,75 +7,103 @@
    :init init))
 
 (defn -init []
-  [[] (ref (org.kuali.maven.wagon.S3Wagon.))])
+  [[] (atom {:kuali (org.kuali.maven.wagon.S3Wagon.)})])
 
 (defn -addSessionListener [this listener]
-  (.addSessionListener @(.state this) listener))
+  (log/trace "addSessionListener" listener)
+  (swap! (.state this) assoc-in [:session-listeners listener] listener)
+  (.addSessionListener (:kuali @(.state this)) listener))
 
 (defn -addTransferListener [this listener]
-  (.addTransferListener @(.state this) listener))
+  (log/trace "addTransferListener" listener)
+  (swap! (.state this) assoc-in [:transfer-listeners listener] listener)
+  (.addTransferListener (:kuali @(.state this)) listener))
 
 (defn -connect
   ([this source]
-     (.connect @(.state this) source))
+     (log/trace "connect" source)
+     (.connect (:kuali @(.state this)) source))
   ([this source auth-or-proxy]
-     (.connect @(.state this) source auth-or-proxy))
+     (log/trace "connect" source auth-or-proxy)
+     (.connect (:kuali @(.state this)) source auth-or-proxy))
   ([this source auth proxy]
-     (.connect @(.state this) source auth proxy)))
+     (log/trace "connect" source auth proxy)
+     (.connect (:kuali @(.state this)) source auth proxy)))
 
 (defn -disconnect [this]
-  (.disconnect @(.state this)))
+  (log/trace "disconnect")
+  (.disconnect (:kuali @(.state this))))
 
 (defn -get [this resource-name destination]
-  (.get @(.state this) resource-name destination))
+  (log/trace "get" resource-name destination)
+  (.get (:kuali @(.state this)) resource-name destination))
 
 (defn -getFileList [this destination-directory]
-  (.getFileList @(.state this) destination-directory))
+  (log/trace "getFileList" destination-directory)
+  (.getFileList (:kuali @(.state this)) destination-directory))
 
 (defn -getIfNewer [this resource-name destination timestamp]
-  (.getIfNewer @(.state this) resource-name destination timestamp))
+  (log/trace "getIfNewer" resource-name destination timestamp)
+  (.getIfNewer (:kuali @(.state this)) resource-name destination timestamp))
 
 (defn -getReadTimeout [this]
-  (.getReadTimeout @(.state this)))
+  (log/trace "getReadTimeout")
+  (.getReadTimeout (:kuali @(.state this))))
 
 (defn -getRepository [this]
-  (.getRepository @(.state this)))
+  (log/trace "getRepository")
+  (.getRepository (:kuali @(.state this))))
 
 (defn -getTimeout [this]
-  (.getTimeout @(.state this)))
+  (log/trace "getTimeout")
+  (.getTimeout (:kuali @(.state this))))
 
 (defn -hasSessionListener [this listener]
-  (.hasSessionListener @(.state this) listener))
+  (log/trace "hasSessionListener" listener)
+  (.hasSessionListener (:kuali @(.state this)) listener))
 
 (defn -hasTransferListener [this listener]
-  (.hasTransferListener @(.state this) listener))
+  (log/trace "hasTransferListener" listener)
+  (.hasTransferListener (:kuali @(.state this)) listener))
 
 (defn -isInteractive [this]
-  (.isInteractive @(.state this)))
+  (log/trace "isInteractive")
+  (.isInteractive (:kuali @(.state this))))
 
 (defn -put [this source destination]
-  (.put @(.state this) source destination))
+  (log/trace "put" source destination)
+  (.put (:kuali @(.state this)) source destination))
 
 (defn -putDirectory [this source-directory destination-directory]
-  (.putDirectory @(.state this) source-directory destination-directory))
+  (log/trace "putDirectory" source-directory destination-directory)
+  (.putDirectory (:kuali @(.state this)) source-directory destination-directory))
 
 (defn -removeSessionListener [this listener]
-  (.removeSessionListener @(.state this) listener))
+  (log/trace "removeSessionListener" listener)
+  (swap! (.state this) update-in [:session-listeners] dissoc listener)
+  (.removeSessionListener (:kuali @(.state this)) listener))
 
 (defn -removeTransferListener [this listener]
-  (.removeTransferListener @(.state this) listener))
+  (log/trace "removeTransferListener" listener)
+  (swap! (.state this) update-in [:transfer-listeners] dissoc listener)
+  (.removeTransferListener (:kuali @(.state this)) listener))
 
 (defn -resourceExists [this resource-name]
-  (.resourceExists @(.state this) resource-name))
+  (log/trace "resourceExists" resource-name)
+  (.resourceExists (:kuali @(.state this)) resource-name))
 
 (defn -setInteractive [this interactive]
-  (.setInteractive @(.state this) interactive))
+  (log/trace "setInteractive" interactive)
+  (.setInteractive (:kuali @(.state this)) interactive))
 
 (defn -setReadTimeout [this timeout-value]
-  (.setReadTimeout @(.state this) timeout-value))
+  (log/trace "setReadTimeout" timeout-value)
+  (.setReadTimeout (:kuali @(.state this)) timeout-value))
 
 (defn -setTimeout [this timeout-value]
-  (.setTimeout @(.state this) timeout-value))
+  (log/trace "setTimeout" timeout-value)
+  (.setTimeout (:kuali @(.state this)) timeout-value))
 
 (defn -supportsDirectoryCopy [this]
-  (.supportsDirectoryCopy @(.state this)))
+  (log/trace "supportsDirectoryCopy")
+  (.supportsDirectoryCopy (:kuali @(.state this))))
