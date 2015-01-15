5d563109c0b81f9a7ca41b641a9cfc6139a7f33c
Paul Stadig <paul@stadig.name>
2015-01-15 16:26:34 -0500

    Setup clojars for gpg creds.

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
             :url "http://mozilla.org/MPL/2.0/"}
   :dependencies [[joda-time "2.2"]
                  [pjstadig/maven-s3-wagon "1.3.4" :exclusions [joda-time]]]
-  :repositories [["releases" "https://clojars.org/repo/"]]
+  :repositories [["releases" {:url "https://clojars.org/repo/"
+                              :creds :gpg}]]
   :eval-in :leiningen)
